03de2aef9bd66edd91da37fe0c83df09a74e02a9
lalyod <mpaproject307@gmail.com>
2023-11-16 14:58:04 +0700

    feat: add presence intent bits

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences
   ],
 });
 
@@ -18,7 +19,7 @@ client.commands = new Collection();
 
 async function connect() {
   try {
-    // await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
     new CommandHandler({
       client,
