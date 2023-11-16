205a4b49728dbf692bc07e11f07f026bc5c2990f
lalyod <mpaproject307@gmail.com>
2023-11-16 15:04:52 +0700

    feat: add modal input to mongoose

--- interactions/verification-submit.js
+++ interactions/verification-submit.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 module.exports = {
   name: "verification-modal",
   /**
@@ -7,9 +9,22 @@ module.exports = {
     if (!interaction.isModalSubmit()) return;
 
     let name = interaction.fields.getTextInputValue("name");
-    let phone_number = interaction.fields.getTextInputValue('phone_number');
-    let email = interaction.fields.getTextInputValue('email');
+    let phone_number = interaction.fields.getTextInputValue("phone_number");
+    let email = interaction.fields.getTextInputValue("email");
 
-    console.log({name, phone_number, email});
+    let user = await User.find({ userId: interaction.member.id });
+
+    if(!user.length){
+      new User({
+        userId: interaction.member.id,
+        name,
+        phoneNumber: phone_number,
+        email
+      }).save();
+
+      interaction.reply({content: 'Anda berhasil tervifikasi', ephemeral: true});
+    }
+
+    interaction.reply({content: 'Anda sudah terifikasi', ephemeral: true});
   },
 };
